3e1185c1203db401c755ecc018cce226864b273d
LuishEtec <luishbarreira@yahoo.com>
2025-10-27 13:22:36 -0300

    Revert "Api conectada"
    This reverts commit 53dd0cab28508c0ddabcc6adca5c2aca0e4a3f71.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-import { routes } from './app/app.routes';
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-};
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
